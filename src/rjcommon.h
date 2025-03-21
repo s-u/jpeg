518379bd8cf3ac1a8dd1a396ec2b663695078d5d
Simon Urbanek <simon.urbanek@r-project.org>
2025-03-21 13:56:59 +1300

    make jpeg messages R warnings (closes #4)

--- src/rjcommon.h
+++ src/rjcommon.h
@@ -44,7 +44,7 @@ Rjpeg_output_message (j_common_ptr cinfo)
     char buffer[JMSG_LENGTH_MAX];
     
     (*cinfo->err->format_message) (cinfo, buffer);
-    REprintf("JPEG decompression: %s", buffer);
+    Rf_warning("JPEG decompression: %s", buffer);
 }
 
 struct Rjpeg_error_mgr {
